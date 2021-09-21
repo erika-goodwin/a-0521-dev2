0364012b5e718af24d0e1ead75781da3fd1bd8c5
erika-goodwin <shellvoerika@gmail.com>
2021-09-21 16:36:47 -0700

    updated

--- 101/NoteManager/exercise.js
+++ 101/NoteManager/exercise.js
@@ -8,11 +8,25 @@ formBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (!addItemInput.value) return;
   //   alert("Clicked!");
+
   const newItemText = addItemInput.value;
   const newItem = document.createElement("li");
-  //   console.log(newItemText);
-
   newItem.innerText = newItemText;
   itemUL.appendChild(newItem);
+
+  //   const fa = document.querySelector("i .fa ");
+  var newP = document.createElement("p");
+  //   console.log(newP);
+  var newi1 = document.createElement("i");
+  var newi2 = document.createElement("i");
+
+  newi1.className = "fa fa-pencil-square-o";
+  newi2.className = "fa fa-times";
+
+  newi1.appendChild(newi2);
+  newP.appendChild(newi1);
+  newItem.appendChild(newP);
+
+  newP.style.textAlign = "right";
   addItemInput.value = "";
 });
